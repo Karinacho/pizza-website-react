b81f7ca74359c96a47340af306768ae2f1209e88
Karinacho <kari.cholakova@gmail.com>
2026-02-07 17:56:45 +0200

    backend configurations

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,3 +1,4 @@
-import { app } from './server.js';
+import { app } from './server.ts';
+import { env  } from '../env.ts';
 
-app.listen(3000, () => console.log('Server is running on port 3000'));
+app.listen(env.PORT, () => console.log(`Server is running on port ${env.PORT}`));
---
+++ backend/src/routes/pizzaRoutes.ts
@@ -0,0 +1,20 @@
+import { Router } from 'express';
+
+const router = Router();
+
+router.get('/', (req, res) => {
+    res.status(200).json({message: 'pizza list'})
+})
+
+router.get('/:id', (req, res) => {
+    const id = req.params.id;
+    res.status(200).json({message: `pizza ${id} details`})
+})
+
+router.post('/', (req, res) => {
+    res.status(201).json({message: 'pizza created'});
+})
+
+router.delete('/:id', (req, res) => {
+    res.status(204).json({message: 'pizza deleted'});
+})
